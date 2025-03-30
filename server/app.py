31431292d265c87ede8d52d758756cc7675abfa5
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-03-30 09:48:47 +0300

    geting data from different points

--- server/app.py
+++ server/app.py
@@ -20,19 +20,31 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakes = [bake.to_dict() for bake in Bakery.query.all()]
+    return make_response(jsonify(bakes), 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bake = Bakery.query.get(id)
+
+    if not bake:
+        return make_response(jsonify({"error": "Bakery not found"}), 404)
+
+    return make_response(jsonify(bake.to_dict(rules=("-baked_goods.bakery",))), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    by_price = [bake.to_dict() for bake in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    return make_response(jsonify(by_price), 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    if not most_expensive:
+        return make_response(jsonify({"error": "No baked goods found"}), 404)
+
+    return make_response(jsonify(most_expensive.to_dict()), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
